acf8f8bee39febcf429ec7f1aebe69c24d1d1569
Michael Henning <mh@fivenp.com>
2019-02-18 22:11:59 +0100

    add hotkey navigation

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,71 @@ import React, { Component } from 'react'
 import { Icon, View } from '@allthings/elements'
 import ColorDrop from './containers/ColorDrop'
 import Navbar from './containers/Navbar'
+import { colors } from './utils/palette'
 
 export interface IAppState {
-  readonly activeColor?: string
+  readonly activeColor?: number
 }
+
+const backgroundColors = { white: '#ffffff', ...colors }
 
 class App extends React.Component<IAppState> {
   public readonly state = {
-    activeColor: 'BlackIntense',
+    activeColor: 0,
+    activeBackgroundColor: 0,
   }
+
+  public nextColor = () => {
+    this.state.activeColor !== Object.keys(colors).length - 1 &&
+      this.setState({ activeColor: this.state.activeColor + 1 })
+  }
+
+  public prevColor = () => {
+    this.state.activeColor !== 0 &&
+      this.setState({ activeColor: this.state.activeColor - 1 })
+  }
+
+  public nextBackgroundColor = () => {
+    this.state.activeBackgroundColor !==
+      Object.keys(backgroundColors).length - 1 &&
+      this.setState({
+        activeBackgroundColor: this.state.activeBackgroundColor + 1,
+      })
+  }
+
+  public prevBackgroundColor = () => {
+    this.state.activeBackgroundColor !== 0 &&
+      this.setState({
+        activeBackgroundColor: this.state.activeBackgroundColor - 1,
+      })
+  }
+
+  private checkKey = (e: any) => {
+    switch (e.key) {
+      case 'ArrowUp':
+        this.prevBackgroundColor()
+        break
+      case 'ArrowDown':
+        this.nextBackgroundColor()
+        break
+      case 'ArrowLeft':
+        this.prevColor()
+        break
+      case 'ArrowRight':
+        this.nextColor()
+    }
+  }
+
   public render(): JSX.Element {
-    document.body.style.backgroundColor = '#ffffff'
+    document.body.style.backgroundColor = Object.values(backgroundColors)[
+      this.state.activeBackgroundColor
+    ]
+    document.onkeydown = this.checkKey
+
+    const color = Object.values(colors)[this.state.activeColor]
+    const colorName = Object.keys(colors)[this.state.activeColor]
+
+    console.log(color)
 
     return (
       <View
@@ -35,7 +89,7 @@ class App extends React.Component<IAppState> {
               position: 'relative',
             }}
           >
-            <ColorDrop color="#333333" colorName="colorName" />
+            <ColorDrop color={color} colorName={colorName} />
           </View>
         </View>
       </View>
